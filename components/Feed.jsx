4bfbcf6a57cf9cc7482646f597484d4b6252407e
suryakant thombare <suryakant.thombare@ispace.com>
2023-06-02 15:17:24 +0530

    cross origin

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -27,7 +27,7 @@ const Feed = () => {
   }
 
   const fetchPosts = async () => {
-    const response = await fetch('/api/prompt');
+    const response = await fetch('/api/prompt', {mode: 'cors'});
     const data = await response.json();
     setPosts(data)
   }
