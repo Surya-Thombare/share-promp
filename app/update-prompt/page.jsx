3ac8dc2bd51e348ad53e0bef84ded826af11aa3a
suryakant thombare <suryakant.thombare@ispace.com>
2023-06-27 23:36:02 +0530

    Update post complete

--- app/update-prompt/page.jsx
+++ app/update-prompt/page.jsx
@@ -28,29 +28,30 @@ const editPrompt = () => {
   }, [promptId])
   
 
-//   const createPrompt = async (e) => {
-//     e.preventDefault();
-//     setIsSubmitting(true);
+  const updatePrompt = async (e) => {
+    e.preventDefault();
+    setIsSubmitting(true);
 
-//     try {
-//       const response = await fetch("/api/prompt/new", {
-//         method: "POST",
-//         body: JSON.stringify({
-//           prompt: post.prompt,
-//           userId: promptId,
-//           tag: post.tag,
-//         }),
-//       });
+    if(!promptId) return alert('Prompt ID Not Found')
 
-//       if (response.ok) {
-//         router.push("/");
-//       }
-//     } catch (error) {
-//       console.log(error);
-//     } finally {
-//       setIsSubmitting(false);
-//     }
-//   };
+    try {
+      const response = await fetch(`/api/prompt/${promptId}`, {
+        method: "PATCH",
+        body: JSON.stringify({
+          prompt: post.prompt,
+          tag: post.tag,
+        }),
+      });
+
+      if (response.ok) {
+        router.push("/");
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsSubmitting(false);
+    }
+  };
 
   return (
     <Form
@@ -58,7 +59,7 @@ const editPrompt = () => {
       post={post}
       setPost={setPost}
       submitting={submitting}
-      handleSubmit={() => {}}
+      handleSubmit={updatePrompt}
     />
   );
 };
